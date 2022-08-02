22bbe8f1b0e76d8d5454597262170d0b46f9407b
kairung <kairung@webmail.npru.ac.th>
2022-08-02 15:52:17 +0700

    add .

--- Home.py
+++ Home.py
@@ -7,11 +7,9 @@ st.set_page_config(
      page_title="Ex-stream-ly Cool App",
      page_icon="🧊",
      layout="wide",
-     initial_sidebar_state="expanded",
-     menu_items={
-         'xxx': 'https://www.extremelycoolapp.com/help',
-         'xxx1': "https://www.extremelycoolapp.com/bug",
-         'xxxx2': "# This is a header. This is an *extremely* cool app!"
-     }
+     initial_sidebar_state="expanded"     
  )
+
+
+st.balloons()
 
