15841e34d5f75d74b5cc9960951aacb457a34b7f
kairung <kairung@webmail.npru.ac.th>
2022-08-02 17:13:17 +0700

    add .

--- pages/ByLevel.py
+++ pages/ByLevel.py
@@ -5,6 +5,8 @@ import numpy as np
 st.markdown("# 🎉การวิเคราะห์รายชั้นปี 🎉")
 st.sidebar.markdown("# Page 2 🎉")
 
+st.sidebar.title("ออกแบบสรรถนะ")
+
 df = pd.DataFrame(
     np.random.randn(10, 5),
     columns=('col %d' % i for i in range(5))
