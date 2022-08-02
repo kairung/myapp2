923e0626101ecb33a519915f464a1ea8b35b4dcc
kairung <kairung@webmail.npru.ac.th>
2022-08-02 15:59:21 +0700

    add .

--- Home.py
+++ Home.py
@@ -3,13 +3,6 @@ import pandas as pd
 import numpy as np
 
 st.title('การวิเคราะห์และออกแบบสมรรถของนักเรียน')
-st.set_page_config(
-     page_title="Ex-stream-ly Cool App",
-     page_icon="🧊",
-     layout="wide",
-     initial_sidebar_state="expanded"     
- )
-
 
 st.balloons()
 
