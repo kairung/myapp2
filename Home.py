c13ef056fae2a1da76ad8f24179494a85a5dc698
kairung <kairung@webmail.npru.ac.th>
2022-08-02 15:32:15 +0700

    add .

---
+++ Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('การวิเคราะห์และออกแบบสมรรถของนักเรียน')
+st.set_page_config(
+     page_title="Ex-stream-ly Cool App",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'xxx': 'https://www.extremelycoolapp.com/help',
+         'xxx1': "https://www.extremelycoolapp.com/bug",
+         'xxxx2': "# This is a header. This is an *extremely* cool app!"
+     }
+ )
+
