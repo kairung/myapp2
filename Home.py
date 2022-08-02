fc8dcf21f69c47f9385e158cfe0eb2c25b090ef1
kairung <kairung@webmail.npru.ac.th>
2022-08-02 17:21:58 +0700

    add .

--- Home.py
+++ Home.py
@@ -1,8 +1,22 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
-st.markdown('การวิเคราะห์และออกแบบสมรรถการเรียนด้าน AI')
+st.markdown('#การวิเคราะห์และออกแบบสมรรถการเรียนด้าน AI')
 st.markdown("# Ailca System 🎈🎈")
 st.balloons()
 st.sidebar.markdown("# 🎉หน้าแรก🎉")
+
+data = dict(
+    character=["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
+    parent=["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
+    value=[10, 14, 12, 10, 2, 6, 6, 4, 4])
+
+fig = px.sunburst(
+    data,
+    names='character',
+    parents='parent',
+    values='value',
+)
+fig.show()
--- pages/ByPerson.py
+++ pages/ByPerson.py
@@ -2,8 +2,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.markdown("# การวิเคราะห์รายบุคคล❄️")
-st.sidebar.markdown("# Page 3 ❄️")
+st.markdown("#❄️การวิเคราะห์รายบุคคล❄️")
+st.sidebar.markdown("# รายบุคคล❄️")
+st.sidebar.title("การออกแบบสรรถนะการเรียนรู้ด้านAI 5 ด้าน")
+st.sidebar.button("ออกแบบ")
+st.sidebar.radio("ด้านที่ 1",["สูง","กลาง","ต่ำ"])
+st.sidebar.radio("ด้านที่ 2",["สูง","กลาง","ต่ำ"])
+st.sidebar.radio("ด้านที่ 3",["สูง","กลาง","ต่ำ"])
+st.sidebar.radio("ด้านที่ 4",["สูง","กลาง","ต่ำ"])
+st.sidebar.radio("ด้านที่ 5",["สูง","กลาง","ต่ำ"])
+
 df = pd.DataFrame(
     np.random.randn(10, 5),
     columns=('col %d' % i for i in range(5)))
