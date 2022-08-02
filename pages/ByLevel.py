c645fbf2f88a0c5143d015c9a225624c0518fb4c
kairung <kairung@webmail.npru.ac.th>
2022-08-02 16:56:29 +0700

    add .

--- pages/ByLevel.py
+++ pages/ByLevel.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.markdown("# การวิเคราะห์รายชั้นปี 🎉")
+st.markdown("# 🎉การวิเคราะห์รายชั้นปี 🎉")
 st.sidebar.markdown("# Page 2 🎉")
 
 df = pd.DataFrame(
--- pages/BySchool.py
+++ pages/BySchool.py
@@ -4,12 +4,16 @@ import pandas as pd
 import altair as alt
 
 st.markdown("# การวิเคราะห์รายโรงเรียน❄️")
-st.sidebar.markdown("# Page 4 ❄️")
+st.sidebar.markdown("# 🎈🎈Page 4 ❄️")
 
 df = pd.DataFrame(
    np.random.randn(500, 3),
    columns=['x','y','z']
    )
+
+st.table(df)
+
 c = alt.Chart(df).mark_circle().encode(
    x='x' , y='y' , size='z', color='z', tooltip=['x', 'y', 'z'])
+
 st.altair_chart(c, use_container_width=True)
